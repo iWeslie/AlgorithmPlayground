398b656a2802aa687fb27a5cb24f81889052a7f8
Weslie <iwesliechen@gmail.com>
2019-01-15 23:15:00 +0800

    remove element from sorted array

---
+++ LeetCode/27. Remove Element.playground/Contents.swift
@@ -0,0 +1,17 @@
+class Solution {
+    func removeElement(_ nums: inout [Int], _ val: Int) -> Int {
+        if nums.count <= 1 {
+            return nums.count
+        }
+        var p1 = 0
+        for p2 in 0..<nums.count {
+            if nums[p2] == val {
+                
+            }
+        }
+        return count
+    }
+}
+
+var test = [3,2,2,3]
+Solution().removeElement(&test, 3)
