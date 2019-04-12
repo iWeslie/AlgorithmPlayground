3309ec7d314999e46032a8f17886128f8d814050
Weslie <iwesliechen@gmail.com>
2019-04-12 16:26:28 +0800

    swift LCP

--- LeetCode/14. Longest Common Prefix.playground/Contents.swift
+++ LeetCode/14. Longest Common Prefix.playground/Contents.swift
@@ -3,21 +3,42 @@ class Solution {
 		if strs.count == 0 { return "" }
 		if strs.count == 1 { return strs.first! }
 		
-		let test = strs.first!
-		
-		var middle = test.count
-		
-		let left = test.prefix(middle / 2)
-		for str in strs[1..<strs.count] {
-			str.prefix(middle / 2)
+		var minLength = Int.max
+		for str in strs {
+			minLength = min(minLength, str.count)
 		}
 		
-		return ""
+		var low = 1
+		var high = minLength
+		
+		while low <= high {
+			let middle = (low + high) / 2
+			if hasCommonPrefix(at: middle, for: strs) {
+				low = middle + 1
+			} else {
+				high = middle - 1
+			}
+		}
+		
+		let middle = (low + high) / 2
+		if middle == 0 {
+			return ""
+		}
+		return String(strs[0].prefix(middle + 1))
+	}
+	
+	func hasCommonPrefix(at index: Int, for strings: [String]) -> Bool {
+		let prefix = strings[0].prefix(index + 1)
+		for str in strings {
+			if !str.hasPrefix(prefix) {
+				return false
+			}
+		}
+		return true
 	}
 }
 
-
 let solution = Solution()
-let test = ["flower","flow","flight"]
+let test = ["aa","ab"]
 solution.longestCommonPrefix(test)
 
